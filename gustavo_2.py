e6725d449a4a7004c48fe708e94eb1f469822dcd
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-11-10 13:56:24 -0300

    gustavo_2.py

--- gustavo_2.py
+++ gustavo_2.py
@@ -1,85 +1,100 @@
-from datetime import datetime, timedelta
-from bs4 import BeautifulSoup as soup
-import pandas as pd
 import requests
+import pandas as pd
+import openpyxl
+from bs4 import BeautifulSoup as soup
+from requests import Session
+from datetime import datetime, timedelta
+import io
 
 class ColetorEProcessadorDados:
     def __init__(self, url, headers):
         self.url = url
         self.headers = headers
-        self.session = requests.Session()
+        self.session = Session()
 
-    def buscar_datas(self):
+    def receber_os_dados(self):
         response = self.session.get(self.url, headers=self.headers)
         sopa = soup(response.content, "html.parser")
-        datas = sopa.find("select", {"name": "ctl00$cphMain$ddlIssue"})
-        datas = datas.find_all("option")
-        return [data.text for data in datas]
+        datas = sopa.find("select", {"name": "ctl00$cphMain$ddlIssue"}).find_all("option")
+        return datas
 
-    def recolher_e_processar_dados(self, data):
-        Script_Manager = "ctl00$cphMain$UpdatePanel1|ctl00$cphMain$ddlIssue"
-        User_Name = ""
-        Password = ""
-        Main_Email = ""
+    def lista_datas(self, datas):
+        lista = [data.text for data in datas]
+        data_inicio = (datetime.today() - timedelta(days=397)).strftime("%Y-%m")
+        df_data = pd.DataFrame(lista, columns=["Site"])
+        df_data["Data"] = pd.to_datetime(df_data["Site"])
+        df_data = df_data.loc[df_data["Data"] >= data_inicio, "Site"]
+        df_lista = []
+        return df_data, df_lista
 
-        parametros = {
-            "ctl00$cphMain$ScriptManager1": Script_Manager,
-            "ctl00$UserName": User_Name,
-            "ctl00$Password": Password,
-            "ctl00$cphMain$txtEmail": Main_Email,
-            "__EVENTTARGET": "ctl00$cphMain$ddlIssue",
-            "__EVENTARGUMENT": "",
-            "__LASTFOCUS": "",
-            "__VIEWSTATE": "",
-            "__VIEWSTATEGENERATOR": "",
-            "__ASYNCPOST": "true",
-            "ctl00$cphMain$ddlIssue": data
-        }
+    def recolhimento_dos_dados(self, df_data):
+        df_lista = []
+        for row in df_data:
+            response = self.session.get(self.url, headers=self.headers)
+            sopa = soup(response.content, "html.parser")
+            Script_Manager = "ctl00$cphMain$UpdatePanel1|ctl00$cphMain$ddlIssue"
+            User_Name, Password, Main_Email = "", "", ""
+            Event_Target = sopa.find("input", {"name": "__EVENTTARGET"}).get("value")
+            Event_Argument = sopa.find("input", {"name": "__EVENTARGUMENT"}).get("value")
+            Last_Focus = sopa.find("input", {"name": "__LASTFOCUS"}).get("value")
+            View_State = sopa.find("input", {"name": "__VIEWSTATE"}).get("value")
+            View_State_Generator = sopa.find("input", {"name": "__VIEWSTATEGENERATOR"}).get("value")
+            Async_Post = "true"
+            parametros = {
+                "ctl00$cphMain$ScriptManager1": Script_Manager,
+                "ctl00$UserName": User_Name,
+                "ctl00$Password": Password,
+                "ctl00$cphMain$txtEmail": Main_Email,
+                "__EVENTTARGET": Event_Target,
+                "__EVENTARGUMENT": Event_Argument,
+                "__LASTFOCUS": Last_Focus,
+                "__VIEWSTATE": View_State,
+                "__VIEWSTATEGENERATOR": View_State_Generator,
+                "__ASYNCPOST": Async_Post,
+                "ctl00$cphMain$ddlIssue": row
+            }
+            response = self.session.post(self.url, headers=self.headers, data=parametros)
+            sopa = soup(response.content, "html.parser")
+            table = sopa.find("table", {"class": "DataGrid"})
+            table = io.StringIO(str(table))
+            df = pd.read_html(table)[0]
+            df.columns = df.loc[0]
+            df = df.iloc[1:, :]
+            df = df[["Resin", "Bid"]]  # Correção aqui
+            df["Data"] = pd.to_datetime(row)
+            df = df.rename(columns={"Resin": "indicador", "Bid": "Valor"})
+            df_lista.append(df)
+        return df_lista
 
-        response = self.session.post(self.url, headers=self.headers, data=parametros)
-        sopa = soup(response.content, "html.parser")
-        table = sopa.find("table", {"class": "DataGrid"})
-        df = pd.read_html(table.prettify())[0]
-        df.columns = df.loc[0]
-        df = df.loc[1:, ["Resin", "Bid"]]
-        df["Data"] = data
-        df = df.rename(columns={"Resin": "indicador", "Bid": "Valor"})
-        
-        # Tratamento adicional dos dados, se necessário
-        # ...
-
+    def tratamento_dos_dados(self, df, df_lista):
+        df = pd.concat(df_lista)
+        df.reset_index(drop=True, inplace=True)
+        wb = openpyxl.load_workbook("INTL_TPE.xlsx")
+        sheet = wb.active
+        columns = ["ID", "indicador"]
+        df_excel = pd.read_excel("INTL_TPE.xlsx", header=3, usecols=columns)
+        df_merged = df.merge(df_excel, on="indicador", how="left")
+        df = df_merged[["ID", "Data", "Valor"]]
+        df["Valor"] = df["Valor"].str.replace("$", " ")
+        df["Valor"] = df["Valor"].astype(float)
         return df
 
-    def coletar_e_processar_todos_os_dados(self):
-        datas = self.buscar_datas()
-        df_lista = []
+    def baixar_excel(self, df):
+        dados_excel = pd.ExcelWriter("dados_excel2.xlsx")
+        df.to_excel(dados_excel, sheet_name="dados_excel2", index=False)
+        dados_excel.close()
 
-        for data in datas:
-            df_data = self.recolher_e_processar_dados(data)
-            df_lista.append(df_data)
+    def main(self):
+        dados = self.receber_os_dados()
+        df_data, df_lista = self.lista_datas(dados)
+        df_lista = self.recolhimento_dos_dados(df_data)
+        df = self.tratamento_dos_dados(df_data, df_lista)
+        self.baixar_excel(df)
 
-        df_concatenado = pd.concat(df_lista)
-        df_concatenado.reset_index(drop=True, inplace=True)
-
-        # Tratamento final dos dados, se necessário
-        # ...
-
-        return df_concatenado
-
-    def baixar_excel(self, df, nome_arquivo="dados_excel.xlsx", nome_planilha="dados_excel"):
-        df.to_excel(nome_arquivo, sheet_name=nome_planilha, index=False)
-
-
-def main():
+if __name__ == "__main__":
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4240.193 Safari/537.36"
     }
     url = "https://www.theplasticsexchange.com/Research/WeeklyReview.aspx"
-
     coletor_processador = ColetorEProcessadorDados(url, headers)
-    df_final = coletor_processador.coletar_e_processar_todos_os_dados()
-    coletor_processador.baixar_excel(df_final)
-
-
-if __name__ == "__main__":
-    main()
+    coletor_processador.main()
